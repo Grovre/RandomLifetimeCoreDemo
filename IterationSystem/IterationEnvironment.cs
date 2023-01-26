a5ceb4ee1501d26c1518a0af830ddc2e3d51d454
Grovre <50428844+Grovre@users.noreply.github.com>
2023-01-26 13:30:05 -0600

    Using iteration environment instead of for loop

--- IterationSystem/IterationEnvironment.cs
+++ IterationSystem/IterationEnvironment.cs
@@ -2,18 +2,34 @@
 
 public class IterationEnvironment
 {
-    public TimeSpan RunTime { get; set; }
     public Action? MillisecondIterationActions { get; set; }
     public bool ContinueLooping { get; set; }
+    public TimeSpan IterationInterval { get; set; }
+    private readonly SemaphoreSlim _envGate;
+    private readonly Timer _openGateTimer;
 
-    public IterationEnvironment(TimeSpan timeUntilStop, params Action[] actions)
+    public IterationEnvironment(TimeSpan iterationInterval, params Action[] actions)
     {
-        RunTime = timeUntilStop;
+        IterationInterval = iterationInterval;
+        _envGate = new(1, 1);
         ContinueLooping = true;
         foreach (var action in actions)
         {
             MillisecondIterationActions += action;
         }
+
+        _openGateTimer = new(_ =>
+        {
+            try
+            {
+                _envGate.Release();
+            }
+            catch (SemaphoreFullException)
+            {
+                // Doesn't need to crash a program,
+                // works as intended
+            }
+        }, null, TimeSpan.Zero, iterationInterval);
     }
 
     private void IterationAction()
@@ -23,7 +39,10 @@ public class IterationEnvironment
             Console.Error.WriteLine("WARNING: No actions to complete in iteration environment");
             Console.Error.WriteLine("Starting empty iteration loop...");
             // ReSharper disable once EmptyEmbeddedStatement
-            while (ContinueLooping) ;
+            while (ContinueLooping)
+            {
+                _envGate.Wait();
+            }
 
             return;
         }
@@ -31,19 +50,27 @@ public class IterationEnvironment
         while (ContinueLooping)
         {
             MillisecondIterationActions.Invoke();
+            _envGate.Wait();
         }
     }
 
-    public Task BeginThread()
+    public void ChangeInterval(TimeSpan interval)
+    {
+        IterationInterval = interval;
+        _openGateTimer.Change(TimeSpan.Zero, interval);
+    }
+
+    public void BeginThread()
     {
         var thread = new Thread(IterationAction);
         thread.Start();
-        return Task.Delay(RunTime).ContinueWith(_ => ContinueLooping = false);
     }
 
+    /// <summary>
+    /// Use with caution. *Will not exit on its own.
+    /// </summary>
     public void BeginBlocking()
     {
-        Task.Delay(RunTime).ContinueWith(_ => ContinueLooping = false);
         IterationAction();
     }
 }
--- RandomLifetimeCoreDemo/Program.cs
+++ RandomLifetimeCoreDemo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
+using IterationSystem;
 using RandomLifetimeCoreDemo.Living;
 using RandomLifetimeCoreDemo.Other.Audio;
+using RandomLifetimeCoreDemo.Other.Helpers;
 using SimpleLogger;
 
 namespace RandomLifetimeCoreDemo;
@@ -9,30 +11,50 @@ public static class Program
 {
     public static void Main()
     {
-        Logger.SharedConsoleLogger.Redirect(Console.Out, true);
-        Console.WriteLine($"PID: {Environment.ProcessId}");
-        var deathChecker = new ParallelLifetimeDeathChecker(TimeSpan.FromMilliseconds(500));
+        // Magic numbers unmagicked
+        var longestTimeUntilFireworkExplosion = TimeSpan.FromSeconds(5);
+        var timeUntilFastExplosions = TimeSpan.FromSeconds(15);
+        var fastExplosionLength = TimeSpan.FromSeconds(10);
+        var iterationEnvInterval = TimeSpan.FromMilliseconds(1000);
+        var deathCheckerInterval = TimeSpan.FromMilliseconds(250);
+        
+        // Firework sounds
         var fireworkSoundDir = $"{new DirectoryInfo(Directory.GetCurrentDirectory())
             .Parent!
             .Parent!
             .Parent!
             .FullName}\\FireworkSoundEffects";
         Console.WriteLine($"Sound effect dir abs path: {fireworkSoundDir}");
-        
         var sounds =
             new WavFiles(fireworkSoundDir);
         
-        const int fireworksToExplode = 60;
-        const int secondsForFireworksToExplode = 60;
-        for (var i = 0; i < fireworksToExplode; i++)
-        {
-            var fw = Firework.Random(Random.Shared, TimeSpan.FromSeconds(secondsForFireworksToExplode));
-            fw.OnDeath += () => sounds.PlayRandom(Random.Shared);
-            deathChecker.BeginWatching(fw);
-        }
+        // Logger and visible startup actions
+        Logger.SharedConsoleLogger.Redirect(Console.Out, true);
+        Console.WriteLine($"PID: {Environment.ProcessId}");
+        var deathChecker = new ParallelLifetimeDeathChecker(deathCheckerInterval);
 
+        // Iteration environment setup
+        var random = new Random();
+        var iterEnv = new IterationEnvironment(iterationEnvInterval);
+        // Iter env: adding log and firework generator action
+        iterEnv.MillisecondIterationActions += 
+            () => Logger.SharedConsoleLogger.Log("Next env iteration beginning");
+        iterEnv.MillisecondIterationActions += () =>
+        {
+            if (!random.NextBool(2, 3)) return;
+            var randomFirework = Firework.Random(random, longestTimeUntilFireworkExplosion);
+            randomFirework.OnDeath += () => sounds.PlayRandom(random);
+            deathChecker.BeginWatching(randomFirework);
+        };
+        
+        // Controls the speed and timings
         deathChecker.StartWatchThread();
-        Thread.Sleep(TimeSpan.FromSeconds(secondsForFireworksToExplode) + TimeSpan.FromMilliseconds(sounds.MsBeforeDisposingPlayer));
+        iterEnv.BeginThread();
+        Task.Delay(timeUntilFastExplosions).Wait();
+        iterEnv.ChangeInterval(TimeSpan.FromMilliseconds(150));
+        Task.Delay(fastExplosionLength).Wait();
+        iterEnv.ContinueLooping = false;
         deathChecker.StopWatchThread();
+        Task.Delay(sounds.MsBeforeDisposingPlayer).Wait(); // For not ending sounds immediately
     }
 }
