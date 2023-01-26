53f390848d705cd6aef033759a019cbf5d2e475c
Grovre <50428844+Grovre@users.noreply.github.com>
2023-01-25 22:41:14 -0600

    ObjectPool.cs

---
+++ RandomLifetimeCoreDemo/Other/ObjectPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+
+namespace RandomLifetimeCoreDemo.Other;
+
+public sealed class ObjectPool<T>
+{
+    private ConcurrentBag<T> _bag;
+    private Func<T> _gen;
+
+    public ObjectPool(Func<T> generator, int initialIdleCount = 0)
+    {
+        _gen = generator;
+        for (var i = 0; i < initialIdleCount; i++)
+        {
+            _bag.Add(_gen());
+        }
+    }
+
+    public T Rent()
+    {
+        var retrieved = _bag.TryTake(out var o);
+        if (!retrieved || o == null)
+        {
+            return _gen();
+        }
+
+        return o;
+    }
+
+    public void Return(T o)
+    {
+        _bag.Add(o);
+    }
+}
